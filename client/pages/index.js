0fcdcfe3b0f97e7b710c36fc302924f6382bbfc7
yomae <goldrunkim@naver.com>
2022-04-25 17:14:33 +0900

    first commit

---
+++ client/pages/index.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <div> Momukzzi 업데이트 합시다! </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
