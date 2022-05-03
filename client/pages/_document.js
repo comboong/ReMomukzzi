15b5beb1650ef419d012a6221a5153bc4c04dc14
Kim Yea Myeong <30408952+yomae@users.noreply.github.com>
2022-05-03 14:18:07 +0900

    add head

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -31,6 +31,7 @@ export default class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 					<style />
 				</Head>
 				<body>
