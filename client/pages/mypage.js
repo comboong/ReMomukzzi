c1ec27e6bea1b88c50226cb32a2e1ffa61dc834d
hyukzz <ri_chard@naver.com>
2022-05-16 21:30:44 +0900

    update: 마이페이지 footer

--- client/pages/mypage.js
+++ client/pages/mypage.js
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Myinfo from "../components/mypage/Myinfo";
+import Footer from "../components/Footer";
 
 const Mypage = () => {
 	return (
 		<>
 			<Header />
 			<Myinfo />
+			<Footer />
 		</>
 	);
 };
