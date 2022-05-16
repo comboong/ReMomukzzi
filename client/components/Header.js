076e4dca166682928176daa8382c8b4c605359d7
hyukzz <ri_chard@naver.com>
2022-05-16 21:35:19 +0900

    update: 닉네임 상태변경

--- client/components/Header.js
+++ client/components/Header.js
@@ -9,189 +9,204 @@ import { useDispatch } from "react-redux";
 import { FavoriteAction } from "../reducers";
 
 const HeaderContainer = styled.div`
-  margin: 0 auto;
-  .navbar {
-    height: 100px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #f1c83e;
-    padding: 9px 12px;
-    min-width: 564px;
+	margin: 0 auto;
+	.navbar {
+		height: 100px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		background-color: #f1c83e;
+		padding: 9px 12px;
+		min-width: 564px;
 
-    @media only screen and (max-width: 804px) {
-      .navlist {
-        margin: 15px 20px;
-      }
-    }
+		@media only screen and (max-width: 804px) {
+			.navlist {
+				margin: 15px 20px;
+			}
+		}
 
-    @media only screen and (max-width: 725px) {
-      .navlist {
-        margin: 15px 15px;
-      }
-    }
+		@media only screen and (max-width: 725px) {
+			.navlist {
+				margin: 15px 15px;
+			}
+		}
 
-    @media only screen and (max-width: 684px) {
-      .navlist {
-        margin: 15px 10px;
-      }
-    }
+		@media only screen and (max-width: 684px) {
+			.navlist {
+				margin: 15px 10px;
+			}
+		}
 
-    @media only screen and (max-width: 642px) {
-      .navlist {
-        margin: 15px 5px;
-      }
-    }
+		@media only screen and (max-width: 642px) {
+			.navlist {
+				margin: 15px 5px;
+			}
+		}
 
-    @media only screen and (max-width: 602px) {
-      .navlist {
-        margin: 15px 0px;
-      }
-    }
-  }
+		@media only screen and (max-width: 602px) {
+			.navlist {
+				margin: 15px 0px;
+			}
+		}
+	}
 
-  .navbar_logo {
-    flex: 1 1 auto;
-  }
+	.navbar_logo {
+		flex: 1 1 auto;
+	}
 
-  .navbar_logo img {
-    width: 200px;
-  }
+	.navbar_logo img {
+		width: 200px;
+	}
 
-  .navbar_menu {
-    display: flex;
-    list-style: none;
-    font-size: 20px;
-    margin: 0px;
-  }
+	.navbar_menu {
+		display: flex;
+		list-style: none;
+		font-size: 20px;
+		margin: 0px;
+	}
 
-  .navbar_menu li {
-    padding: 9px 12px;
-    margin: 15px 30px;
-    cursor: pointer;
-    text-decoration: none;
-  }
+	.navbar_menu li {
+		padding: 9px 12px;
+		margin: 15px 30px;
+		cursor: pointer;
+		text-decoration: none;
+	}
 
-  .navbar_menu li:hover {
-    background-color: #d49466;
-    border-radius: 4px;
-  }
+	.navbar_menu li:hover {
+		background-color: #d49466;
+		border-radius: 4px;
+	}
 
-  .navbar_menu li > div {
-    color: #533026;
-  }
+	.navbar_menu li > div {
+		color: #533026;
+	}
 
-  .navbar_link {
-    text-decoration: none;
-    color: #533026;
-  }
+	.navbar_link {
+		text-decoration: none;
+		color: #533026;
+	}
 
-  .navbar_icons {
-    list-style: none;
-    color: #533026;
-    display: flex;
-    margin-right: 20px;
-  }
+	.navbar_icons {
+		list-style: none;
+		color: #533026;
+		display: flex;
+		margin-right: 20px;
+	}
 
-  .navbar_icons li {
-    padding: 8px 12px;
-  }
-  .navbar_icons li:hover {
-    background-color: #d49466;
-    border-radius: 4px;
-  }
-  .navbar_toggleBtn {
-    position: absolute;
-    right: 32px;
-    width: 23px;
-    display: none;
-  }
-  .person_circle {
-    font-size: 30px;
-  }
-  .navbar_icons > li {
-    cursor: pointer;
-  }
+	.navbar_icons li {
+		padding: 8px 12px;
+	}
+	.navbar_icons li:hover {
+		background-color: #d49466;
+		border-radius: 4px;
+	}
+	.navbar_toggleBtn {
+		position: absolute;
+		right: 32px;
+		width: 23px;
+		display: none;
+	}
+	.person_circle {
+		font-size: 30px;
+	}
+	.navbar_icons > li {
+		cursor: pointer;
+	}
 `;
 
 const Header = () => {
-  const router = useRouter();
-  const dispatch = useDispatch();
+	const router = useRouter();
+	const dispatch = useDispatch();
 
-  const handleLogOut = useCallback(() => {
-    if (window.confirm("로그아웃 하시겠습니까?")) {
-      axios
-        .get(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/logout`, {
-          withCredentials: true,
-        })
-        .then((res) => {
-          Cookies.remove("accessToken");
-          Cookies.remove("nickname");
-          Cookies.remove("email");
-          Cookies.remove("Oauth");
+	const handleLogOut = useCallback(() => {
+		if (window.confirm("로그아웃 하시겠습니까?")) {
+			axios
+				.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/users/logout`, {
+					withCredentials: true,
+				})
+				.then(res => {
+					Cookies.remove("accessToken");
+					Cookies.remove("nickname");
+					Cookies.remove("email");
+					Cookies.remove("Oauth");
 
-          localStorage.setItem("visited", JSON.stringify([]));
+					localStorage.setItem("visited", JSON.stringify([]));
 
-          console.log(res);
-          router.push("/");
-        });
-    }
-  });
+					console.log(res);
+					router.push("/");
+				});
+		}
+	});
 
-  return (
-    <HeaderContainer>
-      <nav className="navbar">
-        <div className="navbar_logo">
-          <Link href="/">
-            <a>로고 들어갈 부분</a>
-          </Link>
-        </div>
-        {Cookies.get("accessToken") ? (
-          <ul className="navbar_menu">
-            <li className="navlist">
-              <div>{Cookies.get("nickname")}님</div>
-            </li>
-            <li className="navlist">
-              <Link href="/mypage" className="navbar_link">
-                <div>마이페이지</div>
-              </Link>
-            </li>
-            <li onClick={handleLogOut} className="navlist">
-              <div>로그아웃</div>
-            </li>
-            <li
-              onClick={() => {
-                dispatch(FavoriteAction(true));
-              }}
-              className="navlist"
-            >
-              <div>즐겨찾기</div>
-            </li>
-          </ul>
-        ) : (
-          <ul className="navbar_menu">
-            <li className="navlist">
-              <Link href="/login">
-                <div>로그인</div>
-              </Link>
-            </li>
-            <li className="navlist">
-              <Link href="/signup">
-                <div>회원가입</div>
-              </Link>
-            </li>
-            <li
-              onClick={() => {
-                dispatch(FavoriteAction(true));
-              }}
-              className="navlist"
-            >
-              <div>즐겨찾기</div>
-            </li>
-          </ul>
-        )}
-      </nav>
-    </HeaderContainer>
-  );
+	const [newNick, setNewNick] = useState();
+	const accessToken = Cookies.get("accessToken");
+
+	useEffect(() => {
+		axios
+			.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/users`, {
+				headers: { authorization: `Bearer ${accessToken}` },
+				"Content-Type": "application/json",
+			})
+			.then(res => {
+				setNewNick(res.data.data.userInfo.nickname);
+			})
+			.catch(err => {});
+	}, []);
+
+	return (
+		<HeaderContainer>
+			<nav className="navbar">
+				<div className="navbar_logo">
+					<Link href="/">
+						<a>로고 들어갈 부분</a>
+					</Link>
+				</div>
+				{Cookies.get("accessToken") ? (
+					<ul className="navbar_menu">
+						<li className="navlist">
+							<div>{newNick}님</div>
+						</li>
+						<li className="navlist">
+							<Link href="/mypage" className="navbar_link">
+								<div>마이페이지</div>
+							</Link>
+						</li>
+						<li onClick={handleLogOut} className="navlist">
+							<div>로그아웃</div>
+						</li>
+						<li
+							onClick={() => {
+								dispatch(FavoriteAction(true));
+							}}
+							className="navlist"
+						>
+							<div>즐겨찾기</div>
+						</li>
+					</ul>
+				) : (
+					<ul className="navbar_menu">
+						<li className="navlist">
+							<Link href="/login">
+								<div>로그인</div>
+							</Link>
+						</li>
+						<li className="navlist">
+							<Link href="/signup">
+								<div>회원가입</div>
+							</Link>
+						</li>
+						<li
+							onClick={() => {
+								dispatch(FavoriteAction(true));
+							}}
+							className="navlist"
+						>
+							<div>즐겨찾기</div>
+						</li>
+					</ul>
+				)}
+			</nav>
+		</HeaderContainer>
+	);
 };
 export default Header;
