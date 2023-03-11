ebcfbf8af255b8890f93189f82763fcb77982a0b
hyukzz <ri_chard@naver.com>
2023-03-12 02:29:43 +0900

    feat: 음식점 정보 반응형 구현

--- client/components/ShopInfo.js
+++ client/components/ShopInfo.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
 
 const ShopInfoDiv = styled.div`
-  padding: 30px;
-  min-height: 200px;
-  min-width: 470px;
+  @media (max-width: 380px) {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
 
   & > table {
     margin: auto;
@@ -30,6 +31,14 @@ const ShopInfoDiv = styled.div`
     vertical-align: top;
     padding-right: 10px;
     padding-bottom: 5px;
+
+    @media (max-width: 380px) {
+      font-size: 0.9rem;
+    }
+
+    @media (max-width: 280px) {
+      font-size: 0.7rem;
+    }
   }
 
   & > table > tbody > tr > td {
@@ -39,6 +48,10 @@ const ShopInfoDiv = styled.div`
     text-align: left;
     vertical-align: middle;
     padding-bottom: 5px;
+
+    @media (max-width: 280px) {
+      font-size: 10px;
+    }
   }
 `;
 
@@ -49,27 +62,29 @@ const ShopInfo = ({ shopInfo }) => {
         <tbody>
           <tr>
             <th>주소</th>
-            <td>{shopInfo.shopInfo.road_address_name}</td>
+            <td>{shopInfo?.shopInfo?.road_address_name}</td>
           </tr>
           <tr>
             <th>음식 종류</th>
-            <td>{shopInfo.shopInfo.category_name.split(">")[1]}</td>
+            <td>{shopInfo?.shopInfo?.category_name?.split(">")[1]}</td>
           </tr>
           <tr>
             <th>전화번호</th>
             <td>
-              {!shopInfo.shopInfo.phone ? "정보 없음" : shopInfo.shopInfo.phone}
+              {!shopInfo?.shopInfo?.phone
+                ? "정보 없음"
+                : shopInfo?.shopInfo?.phone}
             </td>
           </tr>
           <tr>
             <th>메뉴</th>
             <td>
               <ul style={{ margin: 0, padding: 0, listStyle: "none" }}>
-                {shopInfo.shopMenus.slice(0, 8).map((item, idx) => {
+                {shopInfo?.shopMenus?.slice(0, 8).map((menu, i) => {
                   return (
-                    <li key={idx}>
-                      {item[0]} : {item[1]}
-                      {item[1] !== "가격 정보 없음" ? "원" : ""}
+                    <li key={i}>
+                      {menu[0]} : {menu[1]}
+                      {menu[1] !== "가격 정보 없음" ? "원" : ""}
                     </li>
                   );
                 })}
